fa66f4dfc8cf2897ec0cec91a0e0d79cba1a4b23
DeepikaDash <DeepikaDash@users.noreply.github.com>
2016-03-11 16:52:22 +0530

    Update CrossSelling.R

--- CrossSelling.R
+++ CrossSelling.R
@@ -1,46 +1,49 @@
-setwd("C:/Users/ing12709/Desktop/Deepika/")
+setwd("C:/Users/ing12709/Desktop/Deepika/CrossSelling")
 
-list.of.packages=c("randomForest", "caret","dummies","arules")
+list.of.packages <- c("randomForest","caret","dummies","arules", "tidyr")
 
 new.packages<-list.of.packages[!list.of.packages %in% rownames(installed.packages())]
 if(length(new.packages)) install.packages(new.packages)
-library(list.of.packages)
+
+lapply(list.of.packages, library, character.only=T)
 
 rm(list=ls())
 
-ClickStream <- read.delim("ClickStream.txt",stringsAsFactors=FALSE,header = T)
+ClickStream <- read.csv("ClickStream.txt",stringsAsFactors=FALSE,header = T)
 ProductCat <- read.delim("ProductCategoryData.txt",stringsAsFactors=FALSE,header = T)
 UserProfile <- read.delim("UserProfileData.txt",stringsAsFactors=FALSE,header = T)
 
-ClickStream <- ClickStream[,1:7]
+Devide date and Time
+ClickStreamNew=separate(data = ClickStream, col = Timestamp, into = c("Day","Time"), sep = " ")
 
-cat("Clickstream data set property, number of rows: ", nrow(ClickStream), ", number of columns: ", ncol(ClickStream))
-ClickStream_ProductCat <- merge(ClickStream, ProductCat, by = "URL",all.x = T)
+for(i in nrow(ClickStreamNew))
+{
+  if(!all(is.na(as.Date(as.character(ClickStreamNew[i,1]),format="%d/%m/%y"))))
+  {#"9/3/2012"
+    ClickStreamNew[i,1] <- as.character(as.Date(ClickStreamNew[i,1],format ="%d/%m/%Y"))
+  }
+  else
+  {#"22-03-2012"
+    ClickStreamNew[i,1] <- as.character(as.Date(ClickStreamNew[i,1],format ="%d-%m-%Y"))
+  }
+}
+
+cat("Clickstream data set property, number of rows: ", nrow(ClickStreamNew), ", number of columns: ", ncol(ClickStreamNew))
+ClickStream_ProductCat <- merge(ClickStreamNew, ProductCat, by = "URL",all.x = T)
 cat("ClickStream_ProductCat data set property, number of rows: ", nrow(ClickStream_ProductCat), ", number of columns: ", ncol(ClickStream_ProductCat))
 
 ClickStream_ProductCat_UserProfile=merge(ClickStream_ProductCat, UserProfile, by = "User.ID",all.x = T)
 cat("ClickStream_ProductCat_UserProfile data set property, number of rows: ", nrow(ClickStream_ProductCat_UserProfile), ", number of columns: ", ncol(ClickStream_ProductCat_UserProfile))
 
-ClickStream_ProductCat_UserProfile_Cleaned <- ClickStream_ProductCat_UserProfile[,c(-2,-4,-5)]
-cat("ClickStream_rmuw data set property, number of rows: ", nrow(ClickStream_rmuw), ", number of columns: ", ncol(ClickStream_rmuw))
+ClickStream_ProductCat_UserProfile_Cleaned <- ClickStream_ProductCat_UserProfile[,-5]
+cat("ClickStream_ProductCat_UserProfile_Cleaned data set property, number of rows: ", nrow(ClickStream_ProductCat_UserProfile_Cleaned), ", number of columns: ", ncol(ClickStream_ProductCat_UserProfile_Cleaned))
 
 summary(is.na(ClickStream_ProductCat_UserProfile_Cleaned))
 
-ClickStream_ProductCat_UserProfile_Cleaned[ClickStream_ProductCat_UserProfile_Cleaned$College.Education == "Unknown"]=NA
+#ClickStream_ProductCat_UserProfile_Cleaned[ClickStream_ProductCat_UserProfile_Cleaned$College.Education == "Unknown"]=NA
 
-sum(ClickStream_ProductCat_UserProfile_Cleaned == "Unknown",na.rm = T)
+#sum(ClickStream_ProductCat_UserProfile_Cleaned == "Unknown",na.rm = T)
 
 ClickStream_ProductCat_UserProfile_Cleaned$newCol=1
 
 combineData <- reshape(ClickStream_ProductCat_UserProfile_Cleaned[],timevar = "Category",idvar = c("User.ID"),direction = "wide",v.names = "newCol")
-
-combinedData <- spread(ClickStream_ProductCat_UserProfile_Cleaned,Test,Result)
-pMiss <- function(x){sum(is.na(x))/length(x)*100}
-apply(combineData,2,pMiss)
-
-combineData <- combineData[,c(-5,-6)
-combineData[is.na(combineData)] <- 0
-
-toFactor <= function(x){as.factor(x)}
-apply(combineData[,5:17],2,toFactor)
-
